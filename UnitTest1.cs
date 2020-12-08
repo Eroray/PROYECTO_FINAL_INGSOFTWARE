8e7bcf2f94abb4d93b92031861c8e81bd889e123
Eroray <47163565+Eroray@users.noreply.github.com>
2020-12-07 21:44:05 -0600

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,20 +5,21 @@ namespace Program.Unit.Test2
 {
     public class UnitTest2
     {
+         
         //Test 1
         [Theory]
-        [InlineData(3,7,4,2,126)]
-        [InlineData(2,3,5,8,78)]
-        [InlineData(3,4,7,3,120)]
-        [InlineData(4,5,2,9,220)]
-        [InlineData(5,5,7,8,375)]
+        [InlineData(3,7,4,2,108)]
+        [InlineData(2,3,5,8,110)]
+        [InlineData(3,4,7,3,147)]
+        [InlineData(4,5,2,9,112)]
+        [InlineData(5,5,7,8,455)]
 
         public void Test1(double numerodeLados, double basePrisma, double apotemaPrisma, double alturaPrisma, double d) 
         {
             //Datos
-            Calculator  Cal01 =new Calculator();
+            var Cal01 =new Calculator();
             //Actuar
-            double areaPrisma = Cal01.area(numerodeLados, Base Prisma, apotemaPrisma, altura Prisma);
+            double areaPrisma = Cal01.area(numerodeLados, basePrisma, apotemaPrisma, alturaPrisma);
             //Revisar
             Assert.Equal(d, areaPrisma);
         }
@@ -33,31 +34,12 @@ namespace Program.Unit.Test2
         public void Test2(double numerodeLados, double basePrisma, double apotemaPrisma, double alturaPrisma, double d) 
         {
             //Datos
-            Calculator  Cal02 =new Calculator();
+            var Cal02 =new Calculator();
             //Actuar
-            double volumenPrisma = Cal02.volumen(numerodeLados, basePrisma, apotemaPirsma, alturaPrisma);
+            double volumenPrisma = Cal02.volumen(numerodeLados, basePrisma, apotemaPrisma, alturaPrisma);
             //Revisar
             Assert.Equal(d, volumenPrisma);
-        } 
-        
-        //Test 4
-        [Theory]
-        [InlineData(3,7,4,2,85)]
-        [InlineData(2,3,5,8,121)]
-        [InlineData(3,4,7,3,127)]
-        [InlineData(4,5,2,9,181)]
-        [InlineData(5,5,7,8,701)]
-
-        public void Test4(double numerodeLados, double basePrisma, double apotemaPrisma, double alturaPrisma, double d) 
-        {
-            //Datos
-            Calculator  Cal03 =new Calculator();
-            //Actuar
-            double volumenPrisma = Cal03.volumen(numerodeLados, basePrisma, apotemaPrisma, alturaPrisma);
-            //Revisar
-            Assert.NotEqual(d, volumenPrisma);
         }
-        
         //Test 3
         [Theory]
         [InlineData(3,7,4,2,127)]
@@ -69,11 +51,28 @@ namespace Program.Unit.Test2
         public void Test3(double numerodeLados, double basePrisma, double apotemaPrisma, double alturaPrisma, double d) 
         {
             //Datos
-            Calculator  Cal04 =new Calculator();
+            var Cal03 =new Calculator();
             //Actuar
-            double areaPrisma  = Cal04.area(numerodeLados, basePrisma, apotemaPrisma, alturaPrisma);
+            double areaPrisma = Cal03.area(numerodeLados, basePrisma, apotemaPrisma, alturaPrisma);
             //Revisar
             Assert.NotEqual(d, areaPrisma);
+        }
+        //Test 4
+        [Theory]
+        [InlineData(3,7,4,2,85)]
+        [InlineData(2,3,5,8,121)]
+        [InlineData(3,4,7,3,127)]
+        [InlineData(4,5,2,9,181)]
+        [InlineData(5,5,7,8,701)]
+
+        public void Test4(double numerodeLados, double basePrisma, double apotemaPrisma, double alturaPrisma, double d) 
+        {
+            //Datos
+            var Cal04 =new Calculator();
+            //Actuar
+            double volumenPrisma = Cal04.volumen(numerodeLados, basePrisma, apotemaPrisma, alturaPrisma);
+            //Revisar
+            Assert.NotEqual(d, volumenPrisma);
         }
     }
 }
